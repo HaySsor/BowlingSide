ff8890829beff8d236162cd235a85e61ea576707
Przemek <Hayss007@gmail.com>
2022-03-02 21:12:35 +0100

    add new section, try fix Safari fix scroll

---
+++ js/dist/slick.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".accessories__img-boxs").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: false,
+    mobileFirst: true,
+    responsive: [{
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }]
+  });
+});
